bbb73a3854d1a237e97753d40fb4884057c24896
voldi <68856495+voldi-ux@users.noreply.github.com>
2020-08-02 12:39:24 +0200

    Add files via upload

---
+++ precache-manifest.a23d8ff968756f714e3a4fd626907c09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd42ebc91770b770b58e00de53c902b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac50542ce2f530361afd",
+    "url": "/static/css/main.d0772218.chunk.css"
+  },
+  {
+    "revision": "af4c9cfdac3851daf9c2",
+    "url": "/static/js/2.851ac4fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.851ac4fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac50542ce2f530361afd",
+    "url": "/static/js/main.c78c0ce5.chunk.js"
+  },
+  {
+    "revision": "de0170739274e187600c",
+    "url": "/static/js/runtime-main.c1f0c216.js"
+  }
+]);
